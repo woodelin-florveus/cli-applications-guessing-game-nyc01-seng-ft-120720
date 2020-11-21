ad911d2614c6301a718eda6a9b73755d827a2de2
Woodelin Florveus <woodelinflorveus@gmail.com>
2020-11-21 00:54:48 +0000

    Automatically backed up by Learn

--- guessing_game_cli.rb
+++ guessing_game_cli.rb
@@ -7,11 +7,10 @@ def run_guessing_game
       puts "You guessed the correct number!"
     elsif user_input == "exit"
       puts "Goodbye!"
-    else 
+    else
       puts "Sorry! The computer guessed #{rand_sum}."
     end
-
-
-
-
   end
+
+
+# issue user input always a string 
