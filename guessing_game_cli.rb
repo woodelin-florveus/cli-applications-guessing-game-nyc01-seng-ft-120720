af839627007dbb6599baa34a9f46ad27fffd9b73
Woodelin Florveus <woodelinflorveus@gmail.com>
2020-11-21 00:49:17 +0000

    Automatically backed up by Learn

--- guessing_game_cli.rb
+++ guessing_game_cli.rb
@@ -3,7 +3,7 @@ def run_guessing_game
   puts "Guess a number between 1 and 6.\n"
   rand_sum = rand(6) + 1
   user_input = gets.chomp
-    if user_input == rand_sum
+    if user_input == rand_sum.to_s
       puts "You guessed the correct number!"
     elsif user_input != rand_sum
       puts "Sorry! The computer guessed #{rand_sum}."
